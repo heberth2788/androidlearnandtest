e9f15dde28c08179fc8e1c367b4b3bdd6cb56d40
Heberth Deza <heberth2788@gmail.com>
2023-12-27 22:51:44 -0500

    .merge method from Flow with more examples

--- app/src/main/java/com/bbs/learnandtest/flowscombinezipmerge/ViewModelMerge.kt
+++ app/src/main/java/com/bbs/learnandtest/flowscombinezipmerge/ViewModelMerge.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class ViewModelMerge : ViewModel() {
 
@@ -26,6 +30,25 @@ class ViewModelMerge : ViewModel() {
     init {
         merge(flow1, flow2).onEach {
             intStr += "($it)\n"
-        }
+        }.launchIn(viewModelScope)
+
+        // IMPORTANT: The code below do the same as above
+        /*viewModelScope.launch {
+            merge(flow1, flow2).onEach {
+                intStr += "($it)\n"
+            }.collect()
+        }*/
+
+        // Assigning the Flow.merge() to a variable, first way:
+        /*val togetherAux1: Flow<Int> = merge(flow1, flow2).onEach { intStr += "($it)\n" }
+        togetherAux1.launchIn(viewModelScope)*/
+
+        // Assigning the Flow.merge() to a variable, second way:
+        /*viewModelScope.launch {
+            val togetherAux2: Flow<Int> = merge(flow1, flow2).onEach { intStr += "($it)\n" }
+            togetherAux2.collect {
+                print(it)
+            }
+        }*/
     }
 }
